fb15988c8a031df3c82b18158f47259674e86de3
Trostetsovsky <pashorik@mail.ru>
2022-06-24 21:14:00 +0300

    Start creating a User repository

--- endpoints/depends.py
+++ endpoints/depends.py
@@ -4,6 +4,7 @@ from starlette import status
 from db.base import database
 from repositories.locations_repository import LocationRepository
 from repositories.places_repository import PlaceRepository
+from repositories.users_repository import UserRepository
 
 not_found_exception = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Location not found")
 
@@ -14,3 +15,7 @@ def get_place_repository() -> PlaceRepository:
 
 def get_location_repository() -> LocationRepository:
     return LocationRepository(database)
+
+
+def get_user_repository() -> UserRepository:
+    return UserRepository(database)
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 
-from endpoints import places_endpoints, locations_endpoints
+from endpoints import places_endpoints, locations_endpoints, users_endpoints
 from db.base import database
 
 app = FastAPI()
 app.include_router(places_endpoints.router, prefix='/places', tags=['places', ])
 app.include_router(locations_endpoints.router, prefix='/locations', tags=['locations', ])
+app.include_router(users_endpoints.router, prefix='/users', tags=['users', ])
 
 
 @app.get("/")
